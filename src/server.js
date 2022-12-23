a00ed217c588767edb844135b29ef22584bf067b
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-12-23 13:04:59 -0300

    feat: dot.env.example

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -9,4 +12,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(4000, ()=> console.log("Server Running at port 4000!"));
+app.listen(process.env.PORT, ()=> console.log(`Server Running at port ${process.env.PORT}!`));
